e3ee646bafb1bda0f2aeb55bb92ac260728c2ee2
Clara Alarcon <clara.alarcon@tsoftglobal.com>
2022-10-19 09:21:19 -0300

    agregando paquete browser y amigrando sus test alli

---
+++ app/src/test/java/bc10/alarcon/browser/BrowserActionsTest.java
@@ -0,0 +1,48 @@
+package bc10.alarcon.browser;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BrowserActionsTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.edgedriver().setup();
+
+    }
+    @Test
+    void browserActionTest(){
+        String titulo;
+        driver = new EdgeDriver();
+        //abrir url web 1
+        driver.get("https://www.google.com");
+        titulo = driver.getTitle();
+        System.out.println("El titulo de la url abierta es: " + titulo);
+        //abrir url web 2
+        driver.navigate().to("https://selenium.dev");
+        titulo = driver.getTitle();
+        System.out.println("El titulo de la url abierta es: " + titulo);
+        //Back : volver atras
+        driver.navigate().back();
+        titulo = driver.getTitle();
+        System.out.println("El titulo de la url abierta es: " + titulo);
+        //Forward
+        driver.navigate().forward();
+        titulo = driver.getTitle();
+        System.out.println("El titulo de la url abierta es: " + titulo);
+
+        //dRefresh
+        driver.navigate().refresh();
+
+
+    }
+    @AfterEach
+    void close(){
+        driver.quit();
+    }
+}
---
+++ app/src/test/java/bc10/alarcon/browser/BrowserSwitchingTest.java
@@ -0,0 +1,50 @@
+package bc10.alarcon.browser;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BrowserSwitchingTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.edgedriver().setup();
+    //driver.manage().window().fullscreen();
+    }
+    @Test
+    void browserSwitchingTest() {
+        driver = new EdgeDriver();
+        //numero de ventana del driver
+        String originalWindow = driver.getWindowHandle();
+        driver.navigate().to("https://www.google.com.ar");
+        //creamos new Window y nos cambiamos a esa ventana
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        String window2 = driver.getWindowHandle();
+
+        driver.navigate().to("https:www.google.cl");
+
+        //Cambio a ventana 1
+        driver.switchTo().window(originalWindow);
+        //tab es nueva pestaña y window es nueva ventana
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to("https:selenium.dev");
+
+        //cambio a la ventana 2
+        //validar lo del tab, osea que debe quede en la segunda ventana
+        driver.close();
+        driver.switchTo().window(originalWindow);
+        driver.close();
+        driver.switchTo().window(window2);
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to("https:selenium.dev");
+
+    }
+    /*@AfterEach
+    void close(){
+        driver.quit();
+    }*/
+}
